f167f50e0faddafb336352652ba50fe3f5523d5e
Brandon Taylor <brandon.taylor221@gmail.com>
2017-09-01 16:11:32 -0400

    setindex too

--- src/DotOverloading.jl
+++ src/DotOverloading.jl
@@ -2,9 +2,12 @@ module DotOverloading
 
 import MacroTools
 
+value(s) = :($Val{$(Meta.quot(s))}())
+
 fix_dot(any) = any
 fix_dot(e::Expr) = MacroTools.@match e begin
-    t_.s_ => :($get_field($t, $Val{$(Meta.quot(s))}()))
+    (t_.s_ = a_) => :($set_field!($t, $(value(s)), $a))
+    t_.s_ => :($get_field($t, $(value(s))))
     any_ => any
 end
 
@@ -14,9 +17,17 @@ export get_field
 """
     get_field(t, v)
 
-Overload to implement dot overloading.
+Overload to implement dot overloading [`@overload_dots`](@ref).
 """
 get_field(t, v) = getfield(t, inner_value(v))
+
+"""
+    set_field!(t, v, a)
+
+Overload for dot overloading within [`@overload_dots`](@ref).
+"""
+set_field!(t, v, a) = setfield!(t, inner_value(v), a)
+
 
 export @overload_dots
 """
@@ -26,7 +37,21 @@ Allows for type stable dot-overloading. Walks through an expression and replaces
 `a.b` with `get_field(a, Val{:b}())`. Overload [`get_field`](@ref) for new types.
 
 ```jldoctest
-julia> using DotOverloading, ValuedTuples
+julia> using DotOverloading
+
+julia> mutable struct A
+            b::Int
+            c::String
+        end;
+
+julia> a = A(1, "c");
+
+julia> @overload_dots a.b = 2;
+
+julia> a.b
+2
+
+julia> using ValuedTuples
 
 julia> DotOverloading.get_field(v::ValuedTuple, value) = getindex(v, value);
 
